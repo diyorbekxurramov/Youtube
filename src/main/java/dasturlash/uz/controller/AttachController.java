4fc49a349bfd3123974100df7a65ddc83c9a9b78
diyorbekxurramov <diyobekxurramov309@gmail.com>
2025-08-16 10:58:34 +0500

    not done

--- src/main/java/dasturlash/uz/controller/AttachController.java
+++ src/main/java/dasturlash/uz/controller/AttachController.java
@@ -10,7 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 @RestController
-@RequestMapping("/api/v1/attach")
+@RequestMapping("/api/attach")
+
 public class AttachController {
     @Autowired
     private AttachService attachService;
